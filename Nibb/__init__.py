df12af6033a0edf22b324697d5160adcc20b2e21
Venom Snake <78187975+venomsnake@users.noreply.github.com>
2023-04-08 20:38:02 +0530

    Update __init__.py

--- Nibb/__init__.py
+++ Nibb/__init__.py
@@ -101,7 +101,7 @@ SUPPORT_STAFF = list(
 )  # Remove duplicates by using a set
 # Plugins, DB and Workers
 DB_URI = Config.DB_URI
-#DB_NAME = Config.DB_NAME
+DB_NAME = Config.DB_NAME
 NO_LOAD = Config.NO_LOAD
 WORKERS = Config.WORKERS
 
